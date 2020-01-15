ee2fab0141ebb2c31f184abb789e69d249b0afaa
firos <firas.trabelsi.1@esprit.tn>
2020-01-15 01:22:04 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/ModifierprofilController.java

---
+++ src/com/mycompany/gui/SignInForm.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016, Codename One
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ */
+
+package com.mycompany.gui;
+
+import com.codename1.components.FloatingHint;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.Button;
+import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.TextField;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.util.Resources;
+import com.mycompany.Entite.users;
+import com.mycompany.Service.UsersService;
+/**
+ * Sign in UI
+ *
+ * @author Shai Almog
+ */
+public class SignInForm extends Form {
+
+    public SignInForm(Resources res) {
+        
+        super(new BorderLayout());
+        
+      
+        
+        TextField username = new TextField("", "Username", 20, TextField.ANY);
+        TextField password = new TextField("", "Password", 20, TextField.PASSWORD);
+        
+        Button signIn = new Button("Sign In");
+        Button signUp = new Button("Sign Up");
+        signUp.addActionListener(e -> new SignUpForm(res).show());
+        signUp.setUIID("Link");
+        Label doneHaveAnAccount = new Label("Don't have an account?","HugeDarkLabel");
+        doneHaveAnAccount.getAllStyles().setFgColor(0xEFEFF4);
+        signUp.getAllStyles().setFgColor(0xEFEFF4);
+        
+       
+        String a=username.getText();
+       String b=password.getText();
+       UsersService t=new UsersService();
+        signIn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                 if( t.verifielogin ("fartasa","firo") == 1 ){
+            Dialog.show("ok", "ok", "ok", "ok");
+                    
+                    }
+                 else 
+                         Dialog.show("no", "no", "no", "no");
+            
+            }
+        });
+       
+    }
+    
+    
+ 
+    
+    
+}
